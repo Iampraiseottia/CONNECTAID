cdaaf8f38c9c7dd5aa71336b3abcab1bae0a1322
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2025-04-01 00:40:33 +0100

    clean up of project and project started

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import globalStyle from '../globals.css'
+
+
+const Login = () => {
+  return (
+    <div className=' mt-96 bg-slate-700 text-white text-center text-5xl py-32'>Login</div>
+  )
+}
+
+export default Login; 
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import globalStyle from './globals.css'
+
+
+const App = () => {
+  return (
+    <div className=' mt-96 bg-slate-700 text-white'>My Charity Application</div>
+  )
+}
+
+export default App;
