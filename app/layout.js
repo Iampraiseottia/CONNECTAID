3b983168c22889d1893aad254c673bac3901d5c8
OTTIA PRAISE BETECK <ottiapraise50@gmail.com>
2025-07-25 12:49:21 +0100

    Analytics added to monitor the entire system visitors, pages views and  more

--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,33 @@
+import React from "react";
+import Head from "next/head";
 
-import React from 'react'
-import Head from 'next/head' 
+import { icon } from "@fortawesome/fontawesome-svg-core";
 
-import { icon } from '@fortawesome/fontawesome-svg-core'
+import Metadata from "./components/Metadata";
 
-import Metadata from './components/Metadata';
-
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
-  '/': {
-    title: 'ConnectAID Web Application',
-    description: 'ConnectAID is a charity application where seekers(those in need) of help can find and meet donors (those willing to help) in which they can gain valuable assistance.',
+  "/": {
+    title: "ConnectAID Web Application",
+    description:
+      "ConnectAID is a charity application where seekers(those in need) of help can find and meet donors (those willing to help) in which they can gain valuable assistance.",
   },
   icons: {
-    icon: [
-      '/favicon.ico',
-    ],
-    apple: [
-      '/apple-touch-icon.png',
-    ],
-    shortcut: [
-      '/apple-touch-icon.png',
-    ]
+    icon: ["/favicon.ico"],
+    apple: ["/apple-touch-icon.png"],
+    shortcut: ["/apple-touch-icon.png"],
   },
-  manifest: '/site.webmanifest'
-}
+  manifest: "/site.webmanifest",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
